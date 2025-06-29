858a20ff74d8f6ade85d59d991568e36d5239683
DerCommander323 <volcarlos323@googlemail.com>
2025-06-29 16:48:10 +0200

    Port to 1.21.6

--- common/src/main/java/org/embeddedt/modernfix/ModernFix.java
+++ common/src/main/java/org/embeddedt/modernfix/ModernFix.java
@@ -48,7 +48,7 @@ public class ModernFix {
 
     public ModernFix() {
         INSTANCE = this;
-        if(ModernFixMixinPlugin.instance.isOptionEnabled("feature.snapshot_easter_egg.NameChange") && !SharedConstants.getCurrentVersion().isStable())
+        if(ModernFixMixinPlugin.instance.isOptionEnabled("feature.snapshot_easter_egg.NameChange") && !SharedConstants.getCurrentVersion().stable())
             NAME = "PreemptiveFix";
         ModernFixPlatformHooks.INSTANCE.onServerCommandRegister(ModernFixCommands::register);
     }
