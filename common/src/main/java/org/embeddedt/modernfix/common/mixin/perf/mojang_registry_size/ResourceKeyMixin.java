603c32e57910675135deab0d0fdd187e0ef0755e
DerCommander323 <volcarlos323@googlemail.com>
2025-12-09 23:13:52 +0100

    Update to 1.21.11 proper

--- common/src/main/java/org/embeddedt/modernfix/common/mixin/perf/mojang_registry_size/ResourceKeyMixin.java
+++ common/src/main/java/org/embeddedt/modernfix/common/mixin/perf/mojang_registry_size/ResourceKeyMixin.java
@@ -4,27 +4,26 @@ import it.unimi.dsi.fastutil.objects.Object2ObjectOpenHashMap;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.Identifier;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Invoker;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.util.Map;
 
-/*
 @Mixin(ResourceKey.class)
 public class ResourceKeyMixin<T> {
     private static final Map<Identifier, Map<Identifier, ResourceKey<?>>> INTERNING_MAP = new Object2ObjectOpenHashMap<>();
     @Inject(method = "create(Lnet/minecraft/resources/Identifier;Lnet/minecraft/resources/Identifier;)Lnet/minecraft/resources/ResourceKey;", at = @At("HEAD"), cancellable = true)
-    private static <T> void createEfficient(Identifier parent, Identifier location, CallbackInfoReturnable<ResourceKey<T>> cir) {
+    private static <T> void createEfficient(Identifier parent, Identifier identifier, CallbackInfoReturnable<ResourceKey<T>> cir) {
         synchronized (ResourceKey.class) {
             Map<Identifier, ResourceKey<?>> keys = INTERNING_MAP.computeIfAbsent(parent, k -> new Object2ObjectOpenHashMap<>());
-            ResourceKey<?> key = keys.get(location);
+            ResourceKey<?> key = keys.get(identifier);
             if(key == null) {
-                key = new ResourceKey<>(parent, location);
-                keys.put(location, key);
+                key = ResourceKeyInvoker.mfix$invokeCtor(parent, identifier);
+                keys.put(identifier, key);
             }
-            cir.setReturnValue((ResourceKey<T>)key);
+            cir.setReturnValue((ResourceKey<T>) key);
         }
     }
 }
- */
